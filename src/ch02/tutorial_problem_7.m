94df79c02d5c2aada2f1b6fd156c7ca65573c54d
Sergei Sinyak <serega.belarus@gmail.com>
2016-10-20 12:05:41 +0300

    Add a part2 of the solution of ch02/tut7

--- src/ch02/tutorial_problem_7.m
+++ src/ch02/tutorial_problem_7.m
@@ -1,5 +1,7 @@
 % ch02/tutorial_problem_7.m
 
+% Part1
+
 % x[n] = delta[n]
 
 n = (0:20);
@@ -25,3 +27,24 @@ endfor
 
 subplot(212);
 stem(n, y); xlabel('n'); ylabel('h[n - 5]');
+
+% Part2
+
+n = (0:20);
+x1 = (n == 0); % delta[n]
+x2 = ((n - 5) == 0); % delta[n - 5]
+
+y1 = zeros(size(x1));
+y2 = zeros(size(x2));
+
+y1(1) = x1(1);
+y2(1) = x2(1);
+
+for k = 1:20
+  y1(k + 1) = 0.9 * y1(k) + x1(k + 1);
+  y2(k + 1) = 0.9 * y2(k) + x2(k + 1);
+endfor
+
+figure();
+subplot(211); stem(n, y1); xlabel('n'); ylabel('h[n]');
+subplot(212); stem(n, y2); xlabel('n'); ylabel('h[n - 5]');
