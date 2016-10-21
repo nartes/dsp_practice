cfdfc5deb74d62d10d58a6f74eaef75100f91d5a
Sergei Sinyak <serega.belarus@gmail.com>
2016-10-21 11:48:27 +0300

    Fix the problem with a dark stripe
      There was inappropriate call of filter, it was replaced
      with filter2, which implements exactly 2-D filtering

--- src/ch02/tutorial_problem_15.m
+++ src/ch02/tutorial_problem_15.m
@@ -6,11 +6,8 @@ bw_img = sum(img, 3) / (255 * 3);
 
 h = ones(3) / 9;
 
-y1 = filter(h, 1, bw_img);
+y1 = filter2(h, bw_img);
 
 subplot(131); imshow(bw_img);
 subplot(132); imshow(y1);
-subplot(133); imshow(filter(ones(5) / 25, 1, bw_img));
-
-% TODO(nartes): need to explain that increasing, dark stripe
-%   at the top of the second and the third images
+subplot(133); imshow(filter2(ones(5) / 25, bw_img));
